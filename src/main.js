9a30b69bf84de2c3fa750a5af52034ce0d17c209
KellyKiiru <infowithkiiru@gmail.com>
2023-07-24 00:57:23 +0300

    comment out all the code

--- src/main.js
+++ src/main.js
@@ -67,18 +67,18 @@
 
 // hiddenElements.forEach((el) => observer.observe(el));
 
-document.addEventListener("DOMContentLoaded", function() {
-  const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-          console.log(entry);
-          if (entry.isIntersecting) {
-              entry.target.classList.add('show');
-          } else {
-              entry.target.classList.remove('show');
-          }
-      });
-  });
+// document.addEventListener("DOMContentLoaded", function() {
+//   const observer = new IntersectionObserver((entries) => {
+//       entries.forEach((entry) => {
+//           console.log(entry);
+//           if (entry.isIntersecting) {
+//               entry.target.classList.add('show');
+//           } else {
+//               entry.target.classList.remove('show');
+//           }
+//       });
+//   });
 
-  const hiddenElements = document.querySelectorAll('.hidden');
-  hiddenElements.forEach((el) => observer.observe(el));
-});
+//   const hiddenElements = document.querySelectorAll('.hidden');
+//   hiddenElements.forEach((el) => observer.observe(el));
+// });
