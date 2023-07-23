68084baf039fe2656390698f9cfcf83b7b38d894
KellyKiiru <infowithkiiru@gmail.com>
2023-07-23 23:39:39 +0300

    add main.js file

--- src/main.js
+++ src/main.js
@@ -1,52 +1,84 @@
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+// //Get the button
+// let mybutton = document.getElementById("btn-back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+// function scrollFunction() {
+//   if (
+//     document.body.scrollTop > 20 ||
+//     document.documentElement.scrollTop > 20
+//   ) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+// // When the user clicks on the button, scroll to the top of the document
+// mybutton.addEventListener("click", backToTop);
 
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// function backToTop() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
 
- AOS.init({
-// Global settings:
-debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-
-
- });
-
- AOS.init({
-  // AOS would work only for windows bigger than or equal to 768px
-  disable: function() { var maxWidth = 768; return window.innerWidth < maxWidth; }
-});
+//  AOS.init({
+// // Global settings:
+// debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
 
 
-// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+//  });
 
-AOS.init({
+//  AOS.init({
+//   // AOS would work only for windows bigger than or equal to 768px
+//   disable: function() { var maxWidth = 768; return window.innerWidth < maxWidth; }
+// });
 
- mirror: true, // whether elements should animate out while scrolling past them          
 
-  });
+// // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
 
-AOS.init({
+// AOS.init({
+
+//  mirror: true, // whether elements should animate out while scrolling past them          
+
+//   });
+
+// AOS.init({
   
-once: false, // whether animation should happen only once - while scrolling down
+// once: false, // whether animation should happen only once - while scrolling down
 
+//   });
+
+
+// const observer = new IntersectionObserver((entries)=>{
+//     entries.forEach((entry)=>{
+//         console.log(entry)
+//         if (entry.isIntersecting){
+//             entry.target.classList.add('show');
+//         } else{
+//             entry.target.classList.remove('show')
+//         }
+//     });
+// });
+
+// const hiddenElements = document.querySelectorAll('.hidden');
+
+// hiddenElements.forEach((el) => observer.observe(el));
+
+document.addEventListener("DOMContentLoaded", function() {
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+          console.log(entry);
+          if (entry.isIntersecting) {
+              entry.target.classList.add('show');
+          } else {
+              entry.target.classList.remove('show');
+          }
+      });
   });
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
+});
